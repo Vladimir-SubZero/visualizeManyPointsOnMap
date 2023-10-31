c810f94281552b83167b2fbc5eb0e4db985ffa0f
VladimirKholodovskiy <silich2@mail.ru>
2023-10-31 11:36:20 +0600

    Изменил версию

--- src/pages/ol/mapUtils/overlayes/tooltips.ts
+++ src/pages/ol/mapUtils/overlayes/tooltips.ts
@@ -1,29 +1,29 @@
-import { h, render } from 'vue';
-import OrderTooltip from '../../components/OrderTooltip.vue';
+// import { h, render } from 'vue';
+// import OrderTooltip from '../../components/OrderTooltip.vue';
 
 
-function createVNode(component: any) {
-  return h('div', [h(component)]);
-} 
-export const addOverlaysComponentsToHTML = (): void => {
+// function createVNode(component: any) {
+//   return h('div', [h(component)]);
+// } 
+// export const addOverlaysComponentsToHTML = (): void => {
   
-  const oldRootOrdersTooltip = document.getElementById('rootOrdersTooltip');
+//   const oldRootOrdersTooltip = document.getElementById('rootOrdersTooltip');
 
-  const rootOrdersTooltip = document.createElement('div');
+//   const rootOrdersTooltip = document.createElement('div');
 
-  const rootOrdersTooltipId = 'rootOrdersTooltip';
+//   const rootOrdersTooltipId = 'rootOrdersTooltip';
 
-  rootOrdersTooltip.setAttribute('id', rootOrdersTooltipId);
+//   rootOrdersTooltip.setAttribute('id', rootOrdersTooltipId);
 
 
-  if (oldRootOrdersTooltip) {
-    document.body.removeChild(oldRootOrdersTooltip);
-  }
+//   if (oldRootOrdersTooltip) {
+//     document.body.removeChild(oldRootOrdersTooltip);
+//   }
 
-  document.body.appendChild(rootOrdersTooltip);
+//   document.body.appendChild(rootOrdersTooltip);
 
-  const vNodeOrdersTooltip = createVNode(OrderTooltip);
+//   const vNodeOrdersTooltip = createVNode(OrderTooltip);
 
-  render(vNodeOrdersTooltip, rootOrdersTooltip);
+//   render(vNodeOrdersTooltip, rootOrdersTooltip);
 
-}
+// }
--- src/pages/services/pinia-stores/interactions/composition-utils/index.ts
+++ src/pages/services/pinia-stores/interactions/composition-utils/index.ts
@@ -1,57 +1,57 @@
-import { onBeforeMount, onUnmounted } from 'vue';
-import { eventEmitter } from '@/features/planning/planning-page-event-emitter';
-import { usePlanningServerDataStore } from '@/features/planning/pages/orders-planning-page/services/pinia-stores/planning-server-data';
-import { useDistributionCentersStore } from '@/common/services/pinia-stores/distribution-centers';
-import {
-  BodyUnscheduledStops,
-  Run,
-  Scene,
-  ScheduledStop,
-  SchedulingZones,
-  Orders,
-} from '@/common/types/dto-types';
-import { gisMapFeaturesOfStopsModule } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/features/stops/featuresModuleStore';
-import {
-  addOrders,
-  loadStopsOnGisMap,
-  updateOrders,
-  removeOrders,
-} from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/source/stops/useActionsOfStops';
-import cloneDeep from 'lodash/cloneDeep';
-import { updatePlanningMap } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/useInitMap';
-import { moduleForManagingRouteLayersOnGisMap } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/layers/moduleForManagingRouteLayersOnGisMap';
-import { useLocationIconsLayer } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/layers/useLocationIconsLayer';
-import { usePlanningUiInteractionStore } from '@/features/planning/pages/orders-planning-page/services/pinia-stores/ui-interactions';
+// import { onBeforeMount, onUnmounted } from 'vue';
+// import { eventEmitter } from '@/features/planning/planning-page-event-emitter';
+// import { usePlanningServerDataStore } from '@/features/planning/pages/orders-planning-page/services/pinia-stores/planning-server-data';
+// import { useDistributionCentersStore } from '@/common/services/pinia-stores/distribution-centers';
+// import {
+//   BodyUnscheduledStops,
+//   Run,
+//   Scene,
+//   ScheduledStop,
+//   SchedulingZones,
+//   Orders,
+// } from '@/common/types/dto-types';
+// import { gisMapFeaturesOfStopsModule } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/features/stops/featuresModuleStore';
+// import {
+//   addOrders,
+//   loadStopsOnGisMap,
+//   updateOrders,
+//   removeOrders,
+// } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/source/stops/useActionsOfStops';
+// import cloneDeep from 'lodash/cloneDeep';
+// import { updatePlanningMap } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/useInitMap';
+// import { moduleForManagingRouteLayersOnGisMap } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/layers/moduleForManagingRouteLayersOnGisMap';
+// import { useLocationIconsLayer } from '@/features/planning/pages/orders-planning-page/modules/planning-map/composition-utils/layers/useLocationIconsLayer';
+// import { usePlanningUiInteractionStore } from '@/features/planning/pages/orders-planning-page/services/pinia-stores/ui-interactions';
 
 
 
 
-export const useInitListeners = (): void => {
+// export const useInitListeners = (): void => {
 
-  const removeOrdersFromGisMap = (ordersIds: number[]): void => {
-    removeOrders(ordersIds);
-  };
-  const addOrdersToGisMap = (orders: Array<Orders>): void => {
-    addOrders(orders);
-  };
+//   const removeOrdersFromGisMap = (ordersIds: number[]): void => {
+//     removeOrders(ordersIds);
+//   };
+//   const addOrdersToGisMap = (orders: Array<Orders>): void => {
+//     addOrders(orders);
+//   };
 
-  const updateOrdersOnGisMap = (data: {
-    newOrders: Array<Orders>;
-    oldOrdersIds?: number[];
-  }): void => {
-    updateOrders(data.newOrders, data.oldOrdersIds);
-  };
+//   const updateOrdersOnGisMap = (data: {
+//     newOrders: Array<Orders>;
+//     oldOrdersIds?: number[];
+//   }): void => {
+//     updateOrders(data.newOrders, data.oldOrdersIds);
+//   };
 
 
-  onBeforeMount(() => {
-    eventEmitter.on('removeOrdersFromGisMap', removeOrdersFromGisMap);
-    eventEmitter.on('addOrdersToGisMap', addOrdersToGisMap);
-    eventEmitter.on('updateOrdersOnGisMap', updateOrdersOnGisMap);
-  });
+//   onBeforeMount(() => {
+//     eventEmitter.on('removeOrdersFromGisMap', removeOrdersFromGisMap);
+//     eventEmitter.on('addOrdersToGisMap', addOrdersToGisMap);
+//     eventEmitter.on('updateOrdersOnGisMap', updateOrdersOnGisMap);
+//   });
 
-  onUnmounted(() => {
-    eventEmitter.removeListener('removeOrdersFromGisMap', removeOrdersFromGisMap);
-    eventEmitter.removeListener('addOrdersToGisMap', addOrdersToGisMap);
-    eventEmitter.removeListener('updateOrdersOnGisMap', updateOrdersOnGisMap);
-  });
-};
+//   onUnmounted(() => {
+//     eventEmitter.removeListener('removeOrdersFromGisMap', removeOrdersFromGisMap);
+//     eventEmitter.removeListener('addOrdersToGisMap', addOrdersToGisMap);
+//     eventEmitter.removeListener('updateOrdersOnGisMap', updateOrdersOnGisMap);
+//   });
+// };
