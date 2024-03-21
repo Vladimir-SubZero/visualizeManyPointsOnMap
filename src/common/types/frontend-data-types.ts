1b0c8a466f072e65d096d39fda19da164e1ee457
VladimirKholodovskiy <silich2@mail.ru>
2024-03-21 09:48:35 +0600

    Добавил тесты для утилит type-check-utils

--- src/common/types/frontend-data-types.ts
+++ src/common/types/frontend-data-types.ts
@@ -1,6 +1,4 @@
 
-
-
 export type JsTypesInLowerCase =
   | 'string'
   | 'number'
---
+++ src/common/utils/type-check-utils/__test__/type-check-utils.spec.ts
@@ -0,0 +1,37 @@
+import { getRealType, isObject } from '@/common/utils/type-check-utils/type-check-utils';
+
+describe('Тесты для type-check-utils', () => {
+  describe('getRealType()', () => {
+    it('Возвращает реальный тип js-объекта, в нижнем регистре', () => {
+      expect(getRealType(undefined)).toBe('undefined');
+      expect(getRealType(true)).toBe('boolean');
+      expect(getRealType(1)).toBe('number');
+      expect(getRealType('')).toBe('string');
+      expect(getRealType({})).toBe('object');
+      expect(getRealType([])).toBe('array');
+      expect(getRealType(null)).toBe('null');
+      expect(getRealType(Symbol('Sym'))).toBe('symbol');
+      expect(getRealType(() => ({}))).toBe('function');
+      expect(getRealType(new Map())).toBe('map');
+      expect(getRealType(new Set())).toBe('set');
+      expect(getRealType(new FormData())).toBe('formdata');
+    });
+  });
+
+  describe('isObject()', () => {
+    const cases: [data: unknown, result: boolean][] = [
+      [{}, true],
+      [{ id: 1 }, true],
+      [new Map(), false],
+      [new Set(), false],
+      [new FormData(), false],
+      ['', false],
+      ['object', false],
+      [555, false],
+    ];
+
+    test.each(cases)('', (data, result) => {
+      expect(isObject(data)).toBe(result);
+    });
+  });
+});
